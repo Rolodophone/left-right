65ccb7e765d8d9239b5732a53f8cec355ac5509d
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-02-20 13:27:11 +0000

    set size of PC window

--- lwjgl3/src/main/kotlin/net/rolodophone/leftright/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/net/rolodophone/leftright/lwjgl3/Lwjgl3Launcher.kt
@@ -8,7 +8,7 @@ import net.rolodophone.leftright.LeftRight
 fun main() {
 	Lwjgl3Application(LeftRight(), Lwjgl3ApplicationConfiguration().apply {
 		setTitle("LeftRight")
-		setWindowedMode(640, 480)
+		setWindowedMode(9 * 60, 16 * 60)
 		setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png")
 	})
 }
