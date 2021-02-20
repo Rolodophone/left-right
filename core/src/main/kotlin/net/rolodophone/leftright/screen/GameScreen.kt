42a6f10a1c5dca783e9b0e364df571f1ee4028d7
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-02-20 13:57:42 +0000

    start GameScreen

--- core/src/main/kotlin/net/rolodophone/leftright/screen/GameScreen.kt
+++ core/src/main/kotlin/net/rolodophone/leftright/screen/GameScreen.kt
@@ -1,7 +1,23 @@
 package net.rolodophone.leftright.screen
 
+import com.badlogic.gdx.graphics.Texture
+import ktx.ashley.entity
 import net.rolodophone.leftright.LeftRight
 
 class GameScreen(game: LeftRight) : LeftRightScreen(game) {
+	private val playerTexture = Texture("graphics/car1.png")
 
+	override fun show() {
+		engine.entity {
+
+		}
+	}
+
+	override fun render(delta: Float) {
+		engine.update(delta)
+	}
+
+	override fun dispose() {
+		playerTexture.dispose()
+	}
 }
