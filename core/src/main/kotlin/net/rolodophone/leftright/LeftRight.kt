c241442945d533e85b99880a72c917b0e8587823
Sergi Lange-Soler <sergi.lange123@gmail.com>
2021-02-20 13:40:06 +0000

    LeftRight.kt

--- core/src/main/kotlin/net/rolodophone/leftright/LeftRight.kt
+++ core/src/main/kotlin/net/rolodophone/leftright/LeftRight.kt
@@ -1,11 +1,46 @@
 package net.rolodophone.leftright
 
+import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.PooledEngine
+import com.badlogic.gdx.Application.LOG_DEBUG
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.app.KtxGame
-import ktx.app.KtxScreen
+import ktx.log.debug
+import ktx.log.logger
+import net.rolodophone.leftright.screen.GameScreen
+import net.rolodophone.leftright.screen.LeftRightScreen
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
-class LeftRight : KtxGame<KtxScreen>() {
+private const val BATCH_SIZE = 1000
+
+private val log = logger<LeftRight>()
+
+class LeftRight : KtxGame<LeftRightScreen>() {
+	val gameViewport = FitViewport(9 * 30f, 16 * 30f)
+	val batch: Batch by lazy { SpriteBatch(BATCH_SIZE) }
+	val engine: Engine by lazy { PooledEngine().apply {
+		//addSystem(PlayerInputSystem(gameViewport))
+		//addSystem(RenderSystem(batch, gameViewport))
+	} }
+
 	override fun create() {
-		setScreen<FirstScreen>()
+		Gdx.app.logLevel = LOG_DEBUG
+
+		addScreen(GameScreen(this))
+		setScreen<GameScreen>()
+	}
+
+	override fun dispose() {
+		log.debug { "Disposing game" }
+
+		super.dispose()
+
+		log.debug {
+			val sb = batch as SpriteBatch
+			"Max sprites in batch: ${sb.maxSpritesInBatch}; size of batch: $BATCH_SIZE"
+		}
+		batch.dispose()
 	}
 }
